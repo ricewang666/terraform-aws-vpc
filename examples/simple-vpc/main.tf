c804441c09187ea14c134f164c19a44461a47407
wangxianmin <382033356@qq.com>
2022-03-10 10:37:52 +0800

    自定义公私子网

--- examples/simple-vpc/main.tf
+++ examples/simple-vpc/main.tf
@@ -3,8 +3,9 @@ provider "aws" {
 }
 
 locals {
-  region = "eu-west-1"
+  region = "ap-east-1"
 }
+
 
 ################################################################################
 # VPC Module
@@ -13,20 +14,26 @@ locals {
 module "vpc" {
   source = "../../"
 
-  name = "simple-example"
+  name = "new-bitcharm"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["${local.region}a"]
+  
+  private_subnets       = ["10.0.21.0/24","10.0.22.0/24"]
+  public_subnets        = ["10.0.101.0/24","10.0.102.0/24"]
+  
 
   enable_ipv6 = true
 
-  enable_nat_gateway = false
-  single_nat_gateway = true
+  enable_nat_gateway = true
+  single_nat_gateway = false
+  #reuse_nat_ips       = true                    # <= Skip creation of EIPs for the NAT Gateways
+  #external_nat_ip_ids = "${aws_eip.nat.*.id}"
+  one_nat_gateway_per_az = false
 
   public_subnet_tags = {
-    Name = "overridden-name-public"
+    Name = "public"
+    Name = "public-2"
   }
 
   tags = {
@@ -35,6 +42,6 @@ module "vpc" {
   }
 
   vpc_tags = {
-    Name = "vpc-name"
+    Name = "new-bitcharm"
   }
 }
